e795f58af674e98ba8a212581c16b421af8fe343
eden-sasson <106542497+eden-sasson@users.noreply.github.com>
2024-06-07 10:23:40 -0400

    Add files via upload

---
+++ FitnessApp/FitnessApp/FitnessApp_App.swift
@@ -0,0 +1,16 @@
+//
+//  FitnessApp.swift
+//  FitnessApp
+//
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp_NeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DialView()
+        }
+    }
+}
